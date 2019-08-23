a587112d0b3a814bbf26664098837e82d3ea94bd
CarnunMP <carnun@hotmail.co.uk>
2019-08-23 08:40:56 +0100

    o-a.js arrays request 3 done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -79,7 +79,7 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter(graduate => graduate.university.includes("Uni")).map(graduate => graduate.university);
 console.log(uni);
 
 
