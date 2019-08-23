1272f0d03448280b18645dee1ebbc95e190717d9
CarnunMP <carnun@hotmail.co.uk>
2019-08-23 09:12:54 +0100

    p.js s3 done

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -27,6 +27,10 @@ CuboidMaker.prototype.getVolume = function() {
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
+CuboidMaker.prototype.getSurfaceArea = function() {
+  return 2 * ( (this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+}
+
 
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
